e22f8c1fbc340e30f3253a8161ba5f837bf26be0
MONSTER <MONSTER@DESKTOP-9EKD0KI>
2022-09-08 23:22:50 +0300

    Add project files.

---
+++ PlusMinus/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PlusMinus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            // List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            List<int> arr = new List<int>() { -4, 3, -9, 0, 4, 1 };
+
+            Result.plusMinus(arr);
+        }
+    }
+}
---
+++ PlusMinus/Result.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlusMinus
+{
+    class Result
+    {
+        /*
+         * Complete the 'plusMinus' function below.
+         *
+         * The function accepts INTEGER_ARRAY arr as parameter.
+         */
+
+        public static void plusMinus(List<int> arr)
+        {
+            float positiveCount = 0;
+            float negativeCount = 0;
+            float zeroCount = 0;
+            for (int i = 0; i < arr.Count; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    positiveCount++;
+                }
+                else if (arr[i] < 0)
+                {
+                    negativeCount++;
+                }
+                else
+                {
+                    zeroCount++;
+                }
+            }
+
+            Console.WriteLine("Positive : "+positiveCount / arr.Count);
+            Console.WriteLine("Negative : "+negativeCount / arr.Count);
+            Console.WriteLine("Zero : "+zeroCount / arr.Count);
+        }
+    }
+}
