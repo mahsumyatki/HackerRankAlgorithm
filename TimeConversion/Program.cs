67c3410eaeefd9ccf203ba42b7d9f9c3585de135
MONSTER <MONSTER@DESKTOP-9EKD0KI>
2022-09-09 16:27:49 +0300

    TimeConversion is solved

---
+++ TimeConversion/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace TimeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+            string s = "07:05:45PM";
+            string result = Result.timeConversion(s);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ TimeConversion/Result.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace TimeConversion
+{
+    class Result
+    {
+        /*
+         * Complete the 'timeConversion' function below.
+         *
+         * The function is expected to return a STRING.
+         * The function accepts STRING s as parameter.
+         */
+        public static string timeConversion(string s)
+        {
+            var date = DateTime.ParseExact(s,"hh:mm:sstt",CultureInfo.InvariantCulture);
+            return $"{date:HH:mm:ss}";
+        }
+
+    }
+}
