7baa8b9731dea7e1eaf1395aa7f762c455f02a41
MONSTER <MONSTER@DESKTOP-9EKD0KI>
2022-09-10 23:21:30 +0300

    FlippingBits is solved

---
+++ FlippingBits/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FlippingBits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = Convert.ToInt32(Console.ReadLine().Trim());
+            for (int countItr = 0; countItr < count; countItr++)
+            {
+                long n = Convert.ToInt64(Console.ReadLine().Trim());
+
+                long result = Result.flippingBits(n);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ FlippingBits/Result.cs
@@ -0,0 +1,18 @@
+﻿namespace FlippingBits
+{
+    class Result
+    {
+        /*
+         * Complete the 'flippingBits' function below.
+         *
+         * The function is expected to return a LONG_INTEGER.
+         * The function accepts LONG_INTEGER n as parameter.
+         */
+        public static long flippingBits(long n)
+        {
+            uint max = uint.MaxValue;
+            return (max - n);
+        }
+
+    }
+}
