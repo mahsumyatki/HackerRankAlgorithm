85a905ffea81c665cee2bb1b9bb2588596608338
MONSTER <MONSTER@DESKTOP-9EKD0KI>
2022-09-10 20:43:20 +0300

    LonelyInteger is solved

---
+++ LonelyInteger/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LonelyInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> arr = new List<int>() { 1, 3,5,7,9,7,5,3,1 };
+            int result = Result.lonelyInteger(arr);
+            Console.WriteLine(result);
+         } 
+    }
+}
---
+++ LonelyInteger/Result.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LonelyInteger
+{
+    class Result
+    {
+        /*
+         * Complete the 'lonelyinteger' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts INTEGER_ARRAY a as parameter.
+         */
+        public static int lonelyInteger(List<int> arr)
+        {
+            var result = 0;
+            foreach (var item in arr)
+            {
+                result ^= item;
+            }
+
+            return result;
+        }
+
+    }
+}
