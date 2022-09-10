da7ade211ef60f08faf49e75aca3392e39b12bcb
MONSTER <MONSTER@DESKTOP-9EKD0KI>
2022-09-11 01:11:33 +0300

    DiagonalDifference is solved

---
+++ DiagonalDifference/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<int>> arr = new List<List<int>>() {
+                new List<int>(){1,2,5},
+                new List<int>(){4,5,6},
+                new List<int>(){7,8,9}
+            };
+
+            int result = Result.diagonalDifference(arr);
+            Console.WriteLine(Math.Abs(result));
+        }
+    }
+}
---
+++ DiagonalDifference/Result.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace DiagonalDifference
+{
+    class Result
+    {
+        /*
+         * Complete the 'diagonalDifference' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+         */
+        public static int diagonalDifference(List<List<int>> arr)
+        {
+            int leftToRight = 0;
+            int rightToLeft = 0;
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                for (int j = 0; j < arr[i].Count; j++)
+                {
+                    if (i==j)
+                    {
+                        leftToRight += arr[i][j];
+                    }
+
+                    if ( i+j == arr.Count-1)
+                    {
+                        rightToLeft += arr[i][j];
+                    }
+                }
+            }
+            return leftToRight-rightToLeft;
+        }
+
+    }
+}
