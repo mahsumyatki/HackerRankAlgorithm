6ff8be9879e16e36fd716b08035fc6300c1afcf6
MONSTER <MONSTER@DESKTOP-9EKD0KI>
2022-09-13 15:25:15 +0300

    CountingSort1 is solved

---
+++ CountingSort1/Result.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace CountingSort1
+{
+    class Result
+    {
+        /*
+         * Complete the 'countingSort' function below.
+         *
+         * The function is expected to return an INTEGER_ARRAY.
+         * The function accepts INTEGER_ARRAY arr as parameter.
+         */
+        public static List<int> countingSort(List<int> arr)
+        {
+            int[] result = new int[arr.Count];
+
+            List<int> resultArr = new List<int>();
+
+            //int count = 0;
+            for (int i = 0; i < arr.Count; i++)
+            {
+                int num = arr[i];
+                result[num] = result[num] + 1;
+            }
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                resultArr.Add(result[i]);
+            }
+
+            return resultArr;
+        }
+
+    }
+}
